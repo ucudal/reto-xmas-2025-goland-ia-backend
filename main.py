c7331d5a47ee55296e677b0bdfbd93524295643b
Diego Zucca <diego.zucca@correo.ucu.edu.uy>
2025-12-11 20:19:22 -0300

    health_check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from sqlalchemy.exc import OperationalError
 
 app = FastAPI()
 
@@ -6,3 +7,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/health")
+def health_check():
+   return {"message": "200 corriendo..."}
+
+
