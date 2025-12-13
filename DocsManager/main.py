f6062bf334429ac0420c3e19dbcbc821c2062835
Germán Adjemian <110827091+Germanadjemian@users.noreply.github.com>
2025-12-13 09:57:27 -0300

    Remove rag_router from FastAPI app
    Removed the inclusion of rag_router from the FastAPI application.

--- DocsManager/main.py
+++ DocsManager/main.py
@@ -4,11 +4,11 @@ load_dotenv()
 from fastapi import FastAPI
 
 from app.api.routes.base import router as base_router
-from app.api.routes.rag_test import router as rag_router
+
 
 app = FastAPI()
 
 app.include_router(base_router)
-app.include_router(rag_router)
+
 
 
