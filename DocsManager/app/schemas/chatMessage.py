647fe5ee0907f0fc6b220e4d92d7ae258851ec87
Germán Adjemian <110827091+Germanadjemian@users.noreply.github.com>
2025-12-15 17:32:23 -0300

    luego borrar el formato de  la respuesta default del asistente (#33)

---
+++ DocsManager/app/schemas/chatMessage.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from uuid import UUID
+from typing import Optional
+
+class UserMessageIn(BaseModel):
+    session_id: Optional[UUID] = None
+    message: str
+
+
+class AssistantMessageOut(BaseModel):
+    message: str
