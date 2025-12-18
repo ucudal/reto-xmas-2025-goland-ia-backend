7f1849b835c8421e585501dcba6fc0325c3f3790
German Adjemian <GERMAN.ADJEMIAN@correo.ucu.edu.uy>
2025-12-17 21:47:48 -0300

    vuelven los endpoints

--- DocsManager/app/core/config.py
+++ DocsManager/app/core/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import field_validator, model_validator
+from pydantic import field_validator, model_validator, Field
 from typing import Optional
 from urllib.parse import quote_plus
 import logging
@@ -29,7 +29,6 @@ class Settings(BaseSettings):
     minio_access_key: str
     minio_secret_key: str
     minio_bucket: str = "documents"
-    minio_folder: str = "rag-docs"
     minio_use_ssl: bool = True
 
     # Database Configuration (for SQLAlchemy)
